092deaf3331fe8b67c42ceb2c86f5fd2f8dace10
Sala K 405 <salak405@salasfr.itm.edu.co>
2026-02-09 09:20:18 -0500

    comite

--- luis.java
+++ luis.java
@@ -1,8 +1,16 @@
 package HIT;
-
+import java.util.Scanner;
 public class luis {
     
-    public static void main(String[] args) {
-        System.out.println("Hola Mundo");
-    }
+    public static void main(String[] args) 
+{
+    System.out.println("Vamos a sumar");
+    int n1 = 0, n2 = 0;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Ingrese el numero 1");
+    n1 = sc.nextInt();
+    System.out.println("Ingrese el numero 2");
+    n2 = sc.nextInt();
+    System.out.println("La suma de los numeros es: " + (n1 + n2));
+}
 }
